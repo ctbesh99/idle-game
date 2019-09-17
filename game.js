2647bd5cdafc4a064ce0283f5490fc14a93f7a0f
Corey <ctbesh99@harrisonburg.k12.va.us>
2019-09-17 09:15:29 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,32 @@
 var score = 0;
+var locked = 'You got a hat!'
 
 postToPage(score);
 
-createButton('Button', btnPress);
+
+createButton('Clicker', btnPress);
 changeTitle('Clicker Game');
 
-setInterval(btnPress, 1000);
+createButton('Clicker2', btnPress2,);
 
-function btnPress() {
-  score++;
+setInterval(btnPress, 1000);
+setInterval(btnPress2, 1000);
+function btnPress2() {
+  score--;
   postToPage(score);
 }
+
+  function btnPress() {
+  score++;
+  postToPage(score);
+
+  if(score > 100) {
+  postToPage(locked)
+}
+
+}
+
+  
+
+
 
