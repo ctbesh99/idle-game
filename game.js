b6b4a621d0eba4c0a75db6a060bfea0873f20f09
Corey <ctbesh99@harrisonburg.k12.va.us>
2019-09-19 09:44:56 -0400

    Update game.js
    This is the summery!

--- game.js
+++ game.js
@@ -1,53 +1,51 @@
+var score = 0; 
 
-var score = 0;
-var locked = 'You got a hat!'
+var title = new Title('Start by getting 20 clicks!'); 
+var button = new Button('Click+', btnPress);   
+var scr = new Text(score);                    
+let button2 = new Button('Click-', btnPress2); // Make a button
+button2.hide()
+var title2 = new Title('Now Get to -10 clicks');
+title2.hide()
+var title3 = new Title('Great!, Now go back to zero clicks!');
+title3.hide()
+var title4 = new Title('Good Job! Now have fun messing around!');
+title4.hide()
+var title5 = new Title('Acheivement! - Get 50 Clicks!');
+title5.hide()
+var beg = 'beginning'
+                  
+setInterval(btnPress, 1000); 
+setInterval(btnPress2, 1000);  
 
-postToPage(score);
+function btnPress() {
+  score++;        
+  scr.edit(score); 
 
 
-createButton('Clicker', btnPress);
-changeTitle('Clicker Game');
+if(score > 19 && beg == 'beginning') {
+button2.show()
+title.hide()
+title2.show()
+beg = 'mid'
 
-
-
-setInterval(btnPress, 1000);
-setInterval(btnPress2, 1000);
-setInterval(btnPress3, 1000);
-
+}
+if(score == 0 && beg == 'end') {
+  title3.hide()
+  title4.show()
+}
+if(score == 50) {
+  title5.show()
+}
+}
 
 function btnPress2() {
   score--;
-  postToPage(score);
+  scr.edit(score);
+
+  if(score < -9 && beg == 'mid') {
+  title2.hide()
+  title3.show()
+  beg = 'end'
+  }
 }
-
-  function btnPress() {
-  score++;
-  postToPage(score);
-}
-
-  if(score == 100) {
-  postToPage(locked)
-  changeTitle('Now try making it to -1');
-  createButton('subtract', btnPress2,);
-}
-
-
-
-  
-
-
-
-
-var score = 1; // Create a variable to store the score in
-
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var scr = new Text(score);                    // Create a text element
-
-setInterval(btnPress, 1000);   // Set up a loop
-
-function btnPress() {
-  score++;         // Increase the score
-  scr.edit(score); // Update the page with the new score
-}
-
