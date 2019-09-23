c3d5597217a7ca991105884d829949f99b90c87d
Corey <ctbesh99@harrisonburg.k12.va.us>
2019-09-23 08:44:13 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,15 +3,7 @@ var title = new Title('Start by getting 20 clicks!');
 var button = new Button('Click+', btnPress);   
 var scr = new Text(score);                    
 let button2 = new Button('Click-', btnPress2); // Make a button
-button2.hide()
-var title2 = new Title('Now Get to -10 clicks');
-title2.hide()
-var title3 = new Title('Great!, Now go back to zero clicks!');
-title3.hide()
-var title4 = new Title('Good Job! Now have fun messing around!');
-title4.hide()
-var title5 = new Title('Acheivement! - Get 50 Clicks!');
-title5.hide()
+
 var beg = 'beginning'
                   
 setInterval(btnPress, 1000); 
@@ -22,30 +14,7 @@ function btnPress() {
   scr.edit(score); 
 
 
-if(score > 19 && beg == 'beginning') {
-button2.show()
-title.hide()
-title2.show()
-beg = 'mid'
 
-}
-if(score == 0 && beg == 'end') {
-  title3.hide()
-  title4.show()
-}
-if(score == 50) {
-  title5.show()
-}
-}
-
-function btnPress2() {
-  score--;
-  scr.edit(score);
-
-  if(score < -9 && beg == 'mid') {
-  title2.hide()
-  title3.show()
-  beg = 'end'
 
   }
-}
+
