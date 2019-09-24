059de7385470e9fe6fb8580457dd961c9e74e23a
Corey <ctbesh99@harrisonburg.k12.va.us>
2019-09-24 09:52:21 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,12 +2,8 @@ var score = 0;
 var num = 0;
 var title = new Title("Ugg it's 3:00am");
 title.hide()
-var title2 = new Title("'A knock can be heard from the front door'");
+var title2 = new Title("'A scratching noise can be heard coming from the basement!'");
 title2.hide()
-var title3 = new Title("'You go to answer the door'");
-title3.hide()
-var title4 = new Title("'A scratching noise can be heard coming from the basement!'");
-title4.hide()
 var button = new Button('Wake Up', btnPress);   
 var scr = new Text(score);                    
 let button2 = new Button('Next', btnPress2);
@@ -16,6 +12,18 @@ let button3 = new Button('Leave it', btnPress3);
 button3.hide()
 let button4 = new Button('Investigate', btnPress4);
 button4.hide()
+let button5 = new Button('Continue', btnPress5);
+button5.hide()
+let button6 = new Button('Throw away', btnPress6);
+button6.hide()
+let button7 = new Button('Read', btnPress7);
+button7.hide()
+let button8 = new Button('Continue', btnPress8);
+button8.hide()
+let button9 = new Button('Find flashlight', btnPress9);
+button9.hide()
+let button10 = new Button('Go Investigate', btnPress10);
+button10.hide()
 let sub1 = new Button('sub', sub);
 sub1.hide()
 var beg = 'beginning'
@@ -43,31 +51,90 @@ function btnPress2() {
   
 if(score == 2) {
 button2.hide()
-title2.show()
+title.edit("'A knock can be heard from the front door'")
 button2.show()
 }
 if(score == 3) {
 
-  title3.show()
-  button2.hide()
+  title.edit("'You go to answer the door'")
   
-  scr.edit(num);
-if(num == 10) {
-
-  title4.show()
-
-if(num == 300) {
-
+  button2.hide()
+  title2.show()
   button3.show()
   button4.show()
+ 
+
+
+
 
 
 }
 }
 
+function btnPress3() {
+score = 10
+if(score == 10) {
+  title2.hide()
+  title.edit("'You continue to the door and open it. There is a note on the door step.'")
+ button3.hide()
+ button4.hide()
+ button7.show()
+button6.show()
+}
+
+}
+
+function btnPress4() {
+  title2.hide()
+  title.edit("'You open the basement door. Scratch marks cover the backside, but nothing is there.'")
+  button3.hide()
+  button4.hide()
+  button5.show()
+ 
+
+
+}
+
+function btnPress5() {
+  score = 10
+if(score == 10) {
+  button5.hide()
+  title.edit("'You continue to the door and open it. There is a note on the door step.'")
+  button7.show()
+  button6.show()
+
 }
 }
 
+function btnPress6() {
+  button7.hide()
+  button6.hide()
+  title.edit("'You toss the note aside.'")
+  button8.show()
+
+}
+
+function btnPress7() {
+  button7.hide()
+  button6.hide()
+  title.edit("'Dear Jack, thank you for your purchase. We hope you enjoy your new home. Please forget the rumors you might have heard about this place. Your landlord Lary.'")
+
+  button8.show()
+
+}
+
+function btnPress8() {
+  button8.hide()
+  title.edit("'There are now banging noises coming from the basement door!'")
+  button9.show()
+  button10.show()
+}
+
+function btnPress9() {
+
+}
+
+function btnPress10()
 
 score--;
 
